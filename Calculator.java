e622c3723787932cd2c79796e1125296a46eceb7
Josh <dipert@gmail.com>
2019-02-25 20:00:15 -0700

    Step 1

---
+++ Calculator.java
@@ -0,0 +1,60 @@
+package cse360assign2;
+
+public class Calculator {
+
+	private int total;
+	
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	/**
+	 * returns the total after a finite number of calculations are performed.
+	 * @total		The result of the calculations
+	 * @return		Returns the total
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	/**
+	 * adds a value specified by the user to the total
+	 * 
+	 * @param value		Operand to be added to the total
+	 */
+	public void add (int value) {
+	
+	}
+	/**
+	 * Subtracts a value specified by the user against the total
+	 * 
+	 * @param value		Operand to be subtracted
+	 */
+	public void subtract (int value) {
+	
+	}
+	
+	/**
+	 * Multiplies the total by an integer specified by the user
+	 * 
+	 * @param value		Operand to be multiplied by the total
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * divided the total by an integer specified by the user
+	 * 
+	 * @param value		value to be divided int the total
+	 */
+	public void divide (int value) {
+		
+	}
+	/**
+	 * Returns a String that keeps track of all the operations done on the total
+	 * 
+	 * @return
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
